d0895925db43cdb9819b761f2109c90a31d8384c
Richard <richardyangzh@gmail.com>
2020-10-13 15:01:19 -0700

    Task Test 100 coverage

--- src/main/model/Task.java
+++ src/main/model/Task.java
@@ -29,7 +29,7 @@ public class Task {
 
     public Task() throws InvalidDateException {
         setTodayDate();
-        setDueDay(todayDate);
+        setDueDay("NA");
         setCreatedDate();
         setTitle("");
         step = new ArrayList<>();
@@ -39,6 +39,7 @@ public class Task {
         isComplete = false;
         isVisible = true;
     }
+
 
     // MODIFIES: this
     // EFFECTS: add a step to the step list
@@ -52,9 +53,9 @@ public class Task {
     // EFFECTS: If index is invalid, throw InvalidIndexException. Otherwise remove the step
     //          and isStepComplete from the two lists based on the index. Index should start with 0
     public void deleteStep(int index) throws InvalidIndexException {
-        if (index < 0 || step.size() < index - 1) {
-            throw new InvalidIndexException("Index out of bound, there are " + step.size() + " elements. And"
-                    + " index should start with 0");
+        if (index < 0 || step.size() <= index) {
+            throw new InvalidIndexException("Index out of bound, there are " + step.size() + " elements in the list. "
+                    + "And index should start with 0");
         } else {
             step.remove(index);
             isStepComplete.remove(index);
@@ -66,18 +67,35 @@ public class Task {
     // EFFECTS: If index is invalid, throw InvalidIndexException. Otherwise, set the boolean in isStepComplete to true
     public void completeStep(int index) throws InvalidIndexException {
         assert (step.size() == isStepComplete.size());
-        if (index < 0 || step.size() < index - 1) {
-            throw new InvalidIndexException("Index out of bound, there are " + step.size() + " elements. And"
-                                             + " index should start with 0");
+        if (index < 0 || step.size() <= index) {
+            throw new InvalidIndexException("Index out of bound, there are " + step.size() + " elements in the list. "
+                    + "And index should start with 0");
         } else {
             isStepComplete.set(index, Boolean.TRUE);
         }
     }
 
+    // REQUIRES: parameter date has at least 8 digits, and it is positive
+    // EFFECTS: return a String date in the format of "xxxx-xx-xx", year month day.
+    public static String parseDate(int date) {
+        String whole = String.valueOf(date);
+        char[] eachChar = whole.toCharArray();
+        String newWord = "";
+        for (int i = 0; i < 8; i++) {
+            newWord += eachChar[i];
+            if (i == 3 || i == 5) {
+                newWord += "-";
+            }
+        }
+        return newWord;
+    }
+
+    // REQUIRES: parameter date is in the format of year-month-day. year 4 digits, month and day have 2 digits. And the
+    //          total length for the string parameter is 10
     // MODIFIES: the parameter has to have the first 10 substrings in the format of "year-month-day", year month day
     //          numbers, and year has 4 digits, month and day have 2 digits
     // EFFECTS: return the date in String format to 8 digits integer
-    public int decodeDate(String date) {
+    public static int decodeDate(String date) {
         // 8 digits, in the order of year month day, separate with -
         String[] dates = date.split("-");
         return Integer.parseInt(dates[0] + dates[1] + dates[2]);
@@ -91,21 +109,27 @@ public class Task {
         todayDate = today.toString().substring(0, 10);
     }
 
+    // REQUIRES: parameter should be "NA" or a reasonable date. By reasonable,
+    //          it means cannot add date such as 2020-02-31. As February does not have 31th day.
     // MODIFIES: this
-    // EFFECTS: if the due date is 10 year away from today's date, it is an invalid date it will throw
-    // InvalidDateException. if the due date is in the past, set isOverDue to true. Finally, set the due date.
+    // EFFECTS: Set due date to "NA" only if dueDay parameter is exactly "NA".
+    //          Otherwise, if the due date is 10 year away from today's date, it is an invalid date it will throw
+    //          InvalidDateException. if the due date is in the past, set isOverDue to true. Finally, set the due date.
     public void setDueDay(String dueDay) throws InvalidDateException {
-        setTodayDate();
-        int today = decodeDate(todayDate);
-        int due = decodeDate(dueDay);
-        int difference = today - due;
-        if (difference > 100000 || difference < -100000) {
-            throw new InvalidDateException("Invalid date: can only add due date within 10 years");
-        } else {    // due date within 10 years:
-            if (difference > 0) {
-                isOverDue = true;
+        if (dueDay.equals("NA")) {
+            this.dueDay = "NA";
+            isOverDue = false;
+        } else {
+            setTodayDate();
+            int today = decodeDate(todayDate);
+            int due = decodeDate(dueDay);
+            int difference = today - due;
+            if (difference > 100000 || difference < -100000) {
+                throw new InvalidDateException("Invalid date: can only add due date within 10 years");
+            } else {    // due date valid:
+                isOverDue = difference > 0;
+                this.dueDay = dueDay;
             }
-            this.dueDay = dueDay;
         }
     }
 
@@ -115,11 +139,8 @@ public class Task {
         createdDate = todayDate;
     }
 
-    // normal setters methods
 
-    public void setCreatedDate(String createdDate) {
-        this.createdDate = createdDate;
-    }
+    // normal setters methods
 
     public void setImportant(boolean important) {
         isImportant = important;
@@ -141,9 +162,6 @@ public class Task {
         isComplete = complete;
     }
 
-    public void setOverDue(boolean overDue) {
-        isOverDue = overDue;
-    }
 
     // getters methods
 
@@ -188,6 +206,7 @@ public class Task {
     }
 
     public String getTodayDate() {
+        setTodayDate();
         return todayDate;
     }
 
--- src/test/model/TaskTest.java
+++ src/test/model/TaskTest.java
@@ -1,20 +1,260 @@
 package model;
 
 import exceptions.InvalidDateException;
+import exceptions.InvalidIndexException;
 import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskTest {
-    Task task;
+    String todayDate;
+    Task task1;
+    Task task2;
+    Task task3;
 
     @BeforeEach
     public void setup() throws InvalidDateException {
-        task = new Task();
+        setTodayDate();
+        task1 = new Task();
+        task2 = new Task();
+        task3 = new Task("Buy coffee");
+
+        task1.setTitle("This is task 1");
+        task1.setNote("Note for task 1");
+        task1.setImportant(false);
+        task1.setComplete(false);
+        task1.setVisible(true);
+
+        task2.setTitle("This is task 2");
+        task2.setNote("Note for task 2");
+        task2.setImportant(true);
+        task2.setComplete(false);
+        task2.setVisible(false);
+        task2.addStep("Step 1");
+        task2.addStep("Step 2");
+        task2.addStep("Step 3");
+
+        task3.setTitle("This is task 3");
+        task3.setNote("Note for task 3");
+        task3.setImportant(false);
+        task3.setComplete(true);
+        task3.setVisible(true);
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: set today's date to the date in Vancouver timezone with String format like "2020-10-13"
+    public void setTodayDate() {
+        ZoneId zonedId = ZoneId.of("America/Vancouver");
+        LocalDate today = LocalDate.now(zonedId);
+        todayDate = today.toString().substring(0, 10);
     }
 
     @Test
     public void testSetTodayDate() {
-        assertEquals("2020-10-12", task.getTodayDate());
+        task3.setTodayDate();
+        assertEquals(task1.getTodayDate(), task2.getTodayDate());
+        assertEquals(task1.getTodayDate(), task3.getTodayDate());
+        assertEquals(10, task1.getTodayDate().length());
+
+        assertEquals(todayDate, task3.getTodayDate());
     }
 
+    @Test
+    public void testGetters() {
+        assertEquals("This is task 1", task1.getTitle());
+        assertEquals(0, task1.getStep().size());
+        assertEquals(0, task1.getIsStepComplete().size());
+        assertEquals(todayDate, task1.getCreatedDate());
+        assertEquals("NA", task1.getDueDay());
+        assertEquals("Note for task 1", task1.getNote());
+        assertEquals(todayDate, task1.getTodayDate());
+        assertTrue(task1.isVisible());
+        assertFalse(task1.isComplete());
+        assertFalse(task1.isImportant());
+        assertFalse(task1.isOverDue());
+    }
+
+    @Test
+    public void testAddStep() {
+        assertEquals(0, task1.getStep().size());
+        task1.addStep("Step 1");
+        assertEquals(1, task1.getStep().size());
+        assertEquals(false, task1.getIsStepComplete().get(0));
+        assertEquals("Step 1", task1.getStep().get(0));
+    }
+
+    @Test
+    public void testDeleteStep() {
+        assertEquals(3, task2.getStep().size());
+        try {
+            task2.deleteStep(1);
+        } catch (InvalidIndexException invalidIndexException) {
+            fail("This should have not thrown an exception.");
+        }
+        assertEquals(2, task2.getStep().size());
+        assertEquals(2, task2.getIsStepComplete().size());
+        assertEquals("Step 3", task2.getStep().get(1));
+        assertEquals(false, task2.getIsStepComplete().get(1));
+        assertEquals("Step 1", task2.getStep().get(0));
+        assertEquals(false, task2.getIsStepComplete().get(0));
+
+        try {
+            task2.deleteStep(100);
+        } catch (InvalidIndexException invalidIndexException) {
+            System.out.println(invalidIndexException.getMessage());
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            fail("This should have not thrown an exception.");
+        }
+
+
+        try {
+            task2.deleteStep(-4);
+        } catch (InvalidIndexException invalidIndexException) {
+            System.out.println(invalidIndexException.getMessage());
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            fail("This should have not thrown an exception.");
+        }
+
+        try {
+            task2.deleteStep(2);
+        } catch (InvalidIndexException invalidIndexException) {
+            System.out.println(invalidIndexException.getMessage());
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            fail("This should have not thrown an exception.");
+        }
+
+        try {
+            task2.deleteStep(1);
+        } catch (InvalidIndexException | IndexOutOfBoundsException exception) {
+            fail("This should have not thrown an exception.");
+        } finally {
+            assertEquals(1, task2.getStep().size());
+            assertEquals("Step 1", task2.getStep().get(0));
+        }
+    }
+
+    @Test
+    public void testCompleteStep() {
+        try {
+            task2.completeStep(100);
+        } catch (InvalidIndexException invalidIndexException) {
+            System.out.println(invalidIndexException.getMessage());
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            fail("This should have not thrown an exception.");
+        }
+
+
+        try {
+            task2.completeStep(-4);
+        } catch (InvalidIndexException invalidIndexException) {
+            System.out.println(invalidIndexException.getMessage());
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            fail("This should have not thrown an exception.");
+        }
+
+        try {
+            task2.completeStep(3);
+        } catch (InvalidIndexException invalidIndexException) {
+            System.out.println(invalidIndexException.getMessage());
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            fail("This should have not thrown an exception.");
+        }
+
+        assertEquals(false, task2.getIsStepComplete().get(2));
+
+        try {
+            task2.completeStep(2);
+        } catch (InvalidIndexException | IndexOutOfBoundsException exception) {
+            fail("This should have not thrown an exception.");
+        } finally {
+            assertEquals(3, task2.getStep().size());
+            assertEquals(3, task2.getIsStepComplete().size());
+            assertEquals("Step 3", task2.getStep().get(2));
+            assertEquals(true, task2.getIsStepComplete().get(2));
+            assertEquals(false,task2.getIsStepComplete().get(1));
+        }
+
+    }
+
+    @Test
+    public void testDecodeDate() {
+        String testDate1 = "2020-03-06";
+        String testDate2 = "0020-00-02";
+        assertEquals(20200306, Task.decodeDate(testDate1));
+        assertEquals(200002, Task.decodeDate(testDate2));
+    }
+
+    @Test
+    public void testParseDate() {
+        assertEquals("2020-10-13", Task.parseDate(20201013));
+        assertEquals("2234-10-13", Task.parseDate(22341013));
+        assertEquals("2020-10-13", Task.parseDate(2020101365));
+    }
+
+    @Test
+    public void testSetDueDate() {
+        int dueDateTenYearsLaterNum = Task.decodeDate(todayDate) + 100000;
+        String dueDateTenYearsLater = Task.parseDate(dueDateTenYearsLaterNum);
+        try {
+            task1.setDueDay(dueDateTenYearsLater);
+        } catch (InvalidDateException invalidDateException) {
+            fail("This should have not thrown an exception.");
+        } finally {
+            assertEquals(dueDateTenYearsLater, task1.getDueDay());
+            assertEquals(false, task1.isOverDue());
+        }
+
+        int dueDateTenYearsEarlierNum = Task.decodeDate(todayDate) - 100000;
+        String dueDateTenYearsEarlier = Task.parseDate(dueDateTenYearsEarlierNum);
+        try {
+            task1.setDueDay(dueDateTenYearsEarlier);
+        } catch (InvalidDateException invalidDateException) {
+            fail("This should have not thrown an exception.");
+        } finally {
+            assertEquals(dueDateTenYearsEarlier, task1.getDueDay());
+            assertEquals(true, task1.isOverDue());
+        }
+
+        try {
+            task1.setDueDay("NA");
+        } catch (InvalidDateException invalidDateException) {
+            fail("This should have not thrown an exception.");
+        } finally {
+            assertEquals("NA", task1.getDueDay());
+            assertEquals(false, task1.isOverDue());
+        }
+
+        int dueDateTenNOneLaterNum = Task.decodeDate(todayDate) + 100001;
+        String dueDateTenNOneLater = Task.parseDate(dueDateTenNOneLaterNum);
+        try {
+            task2.setDueDay(dueDateTenNOneLater);
+        } catch (InvalidDateException invalidDateException) {
+            invalidDateException.getMessage();
+        } finally {
+            assertEquals("NA", task2.getDueDay());
+            assertEquals(false, task2.isOverDue());
+        }
+
+        int dueDateTenNOneEarlierNum = Task.decodeDate(todayDate) - 100001;
+        String dueDateTenNOneEarlier = Task.parseDate(dueDateTenNOneEarlierNum);
+        try {
+            task2.setDueDay(dueDateTenNOneEarlier);
+        } catch (InvalidDateException invalidDateException) {
+            invalidDateException.getMessage();
+        } finally {
+            assertEquals("NA", task2.getDueDay());
+            assertEquals(false, task2.isOverDue());
+        }
+
+    }
+
+    @Test
+    public void testSetCreatedDate() {
+        task1.setCreatedDate();
+        assertEquals(todayDate, task1.getCreatedDate());
+    }
 }
