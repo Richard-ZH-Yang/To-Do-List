af3fd064d6564c89b7b1479c1988efae1984794a
Richard <richardyangzh@gmail.com>
2020-10-23 22:07:42 -0700

    add json lib

--- src/main/model/BasicList.java
+++ src/main/model/BasicList.java
@@ -8,7 +8,6 @@ import persistence.Writable;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
